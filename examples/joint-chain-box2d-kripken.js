1207ad77250851afebda7659987690874a6a9c82
Erik Weitnauer <eweitnauer@gmail.com>
2012-03-05 18:43:28 +0100

    adapted kripken box2d example, but not working

--- examples/joint-chain-box2d-kripken.js
+++ examples/joint-chain-box2d-kripken.js
@@ -1,9 +1,11 @@
 /// Simulates a chain of sticks connected by ball joints.
+/// Uses the kripken Box2D.js code.
+/// NOT WORKING AT THE MOMENT. Error: assertion I>0.0 failed.
 
 // physics parameters
 
 var world = new Box2D.b2World(new Box2D.b2Vec2(0, 10), true);
-var N = 30,           // number of connected sticks (+ 1 that is fixed)
+var N = 3,           // number of connected sticks (+ 1 that is fixed)
     len = 1,          // object length is 1 m
     mass = 1,         // object mass is 1 kg
     I = 1/3*mass*len, // inertia of object (langer Stab)
@@ -17,7 +19,7 @@ var N = 30,           // number of connected sticks (+ 1 that is fixed)
 var w = 960,    // visualization width in pixels
     h = 600,    // visualization height in pixels
     r = 5,      // radius of joint visualization in pixels
-    scale = 15,  // 1 m in physic = x pixel in visualization
+    scale = 50,  // 1 m in physic = x pixel in visualization
     body_vis, joint_vis; // d3 selections holding the svg visualizations
 
 function init() {
@@ -25,17 +27,18 @@ function init() {
   
   function createBody(x, y, rot, dynamic) {
     var bodyDef = new Box2D.b2BodyDef();
-    bodyDef.position.Set(x, y);
-    bodyDef.angle = rot;
-    bodyDef.type = dynamic ? Box2D.b2Body.b2_dynamicBody : Box2D.b2Body.b2_staticBody;
+    bodyDef.set_position(new Box2D.b2Vec2(x, y));
+    bodyDef.set_angle(rot);
+    bodyDef.set_type(dynamic ? Box2D.b2_dynamicBody : Box2D.b2_staticBody);
     var body = world.CreateBody(bodyDef);
-    var shape = new Box2D.b2PolygonShape.AsBox(len/2, len/2/10);
+    var shape = new Box2D.b2PolygonShape();
+    shape.SetAsBox(len/2, len/2/10);
     var fixtureDef = new Box2D.b2FixtureDef();
-    fixtureDef.restitution = 0;
-    fixtureDef.density = mass/(len*len/10);
-    fixtureDef.friction = 0.5;
-    fixtureDef.shape = shape;
-    fixtureDef.filter.groupIndex = -1; // don't let them collide with each other
+    fixtureDef.set_restitution(0.1);
+    fixtureDef.set_density(mass/(len*len/10));
+    fixtureDef.set_friction(0.5);
+    fixtureDef.set_shape(shape);
+    fixtureDef.get_filter().set_groupIndex(-1); // don't let them collide with each other
     body.CreateFixture(fixtureDef);
     return body;
   }
@@ -49,7 +52,7 @@ function init() {
   bodies.push(createBody(w/scale/2, len, 0, false));
   for (var i=0; i<N; i++) {
     bodies.push(createBody(w/scale/2 + (i+0.5)*len, len + len/2, -Math.PI*0.5, true));
-    joints.push(createJoint(bodies[i], bodies[i+1], w/scale/2 + i*len, len + len/2));
+    //joints.push(createJoint(bodies[i], bodies[i+1], w/scale/2 + i*len, len + len/2));
   }
   
   initial_energy = getTotalEnergy();
@@ -130,8 +133,10 @@ function initVisualization() {
   update();
   
   function dragmove(d, i) {
-    d.SetPosition(new Box2D.b2Vec2(Math.max(r, Math.min(w - r, d3.event.x))/scale,
-                                   Math.max(r, Math.min(h - r, d3.event.y))/scale));
+    d.GetTransform().set_p(new Box2D.b2Vec2(
+      Math.max(r, Math.min(w - r, d3.event.x))/scale,
+      Math.max(r, Math.min(h - r, d3.event.y))/scale));
+                                   
     update();
   }
 }
@@ -139,12 +144,12 @@ function initVisualization() {
 function update() {
   body_vis
     .attr("transform", function(d) {
-      return 'translate(' + d.GetPosition().x*scale + ',' + d.GetPosition().y*scale +
+      return 'translate(' + d.GetPosition().get_x()*scale + ',' + d.GetPosition().get_y()*scale +
              ') rotate(' + 180/Math.PI*d.GetAngle() + ")" })
              
   joint_vis
-    .attr("cx", function(d) { return d.GetAnchorA().x*scale; })
-    .attr("cy", function(d) { return d.GetAnchorA().y*scale; });
+    .attr("cx", function(d) { return d.GetAnchorA().get_x()*scale; })
+    .attr("cy", function(d) { return d.GetAnchorA().get_y()*scale; });
     
   console.log("Total Energy Loss: ", -getTotalEnergy()+initial_energy);
 }
