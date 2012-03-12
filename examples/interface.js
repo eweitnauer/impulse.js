dba9a431558f51a678dd20ee964b0ec558efaab1
Erik Weitnauer <eweitnauer@gmail.com>
2012-03-12 17:47:39 +0100

    complete redesign of the joint-chain example

---
+++ examples/interface.js
@@ -0,0 +1,148 @@
+/// Copyright by Erik Weitnauer, 2012.
+
+/// This file is for creating an interface that lets the user manipulate
+/// the values of parameters. It uses d3 and the range widget of jquery.
+/// The parameters are passed in the following format:
+/// { ID1: {value: 0.1, range:[0,1], label: "param1", postfix: " meters"}
+///  ,ID2: {value: 10, values:[1,10,100,1000]}
+///  ...
+/// }
+/// 'value' is the initial value of the parameter
+/// The value range is either given by 'range:[min,max]' or by a list of allowed
+/// values 'values:[x1,...,xn]'. Optinally, a label and a postfix, that is
+/// displayed behind the value can be passed.
+
+/// Creates an array from a passed parameter hash. The array can be used as
+/// data for d3 selections.
+/// For parameters with a given list of values, the index of the current value
+/// is written to 'idx'. The ids of the parameters are written to 'id'.
+function create_param_array(param_hash) {
+  var a = [];
+  for (var id in param_hash) {
+    var p = param_hash[id];
+    p.id = id;
+    p.step = p.step || 1;
+    if ('values' in p) {
+      p.idx = p.values.indexOf(p.value);
+      p.range = [0, p.values.length-1];
+      p.step = 1; 
+    } else {
+      p.idx = p.value;
+    }
+    a.push(p);
+  }
+  return a;
+}
+
+function addSimulationControls(play_fn, pause_fn, step_fn, stop_fn) {
+  // animation on hover
+  $('#dialog_link, ul#icons li').hover(
+	  function() { $(this).addClass('ui-state-hover'); }, 
+	  function() { $(this).removeClass('ui-state-hover'); }
+  );
+  // add play button
+  $('#play').click(function(evt) {
+    evt.preventDefault();
+    var sp = $("#play span");
+    if (sp.hasClass('ui-icon-play')) {
+      sp.removeClass('ui-icon-play');
+      sp.addClass('ui-icon-pause');
+      $("#play").addClass('ui-state-active');
+      play_fn();
+    } else {
+      sp.removeClass('ui-icon-pause');
+      sp.addClass('ui-icon-play');
+      $("#play").removeClass('ui-state-active');
+      pause_fn();
+    }
+  });
+  // add step button
+  $("#step").click(function(evt) {
+    evt.preventDefault();
+    step_fn();
+  });
+  // add stop button
+  $("#stop_movement").click(function(evt) {
+    evt.preventDefault();
+    stop_fn();
+  });
+}
+
+
+function addParamControls(parent_element, params, update_fn) {
+  var update_params = function() {
+    d3.selectAll("div.parameter")
+    .data(param_array)
+    .select("span")
+    .text(function(d) {
+      if ('values' in d) d.value = d.values[d.idx]
+      else d.value = d.idx;  
+      var str = ('postfix' in d) ? d.postfix : '';
+      return d.value + str;
+    });
+    update_fn();
+  }
+  
+  var number_drag = d3.behavior.drag()
+   .on("drag", function(d) {
+      d3.event.sourceEvent.preventDefault();
+      this.__dx__ += d3.event.dx;
+      var upp = Math.min(20,(d.range[1]-d.range[0])/300);
+      var du = d.step*Math.round(upp*this.__dx__/d.step);
+      var new_val = Math.max(d.range[0],Math.min(d.idx+du, d.range[1]));
+      if (new_val != d.idx) {
+        d.idx = Number(new_val.toFixed(8));
+        this.__dx__ = 0;
+        update_params();
+      }
+    })
+   .on("dragstart", function(d) {
+      d3.select(this).classed('active', true);
+      d3.event.sourceEvent.preventDefault();
+      this.__dx__ = 0;
+    })
+   .on("dragend", function(d) {
+      d3.select(this).classed('active', false);
+      d3.event.sourceEvent.preventDefault();
+      delete this.__dx__;
+    });
+
+  var param_array = create_param_array(params);
+    
+  // add sliders for all parameters
+  var divs = d3.select(parent_element).selectAll("div.parameter")
+    .data(param_array).enter()
+    .append("div")
+    .attr("class", "parameter");
+  
+  divs.append("p")
+    .text(function(d) { return (d.label || d.id) + ': ' })
+    .append("span")
+    .attr("class", function(d) { return d.type=='fixed' ? "" : "changable" })
+    .style("cursor", function(d) { return d.type=='number' ? "ew-resize" : null});
+  
+  d3.selectAll("span.changable")
+    .call(number_drag);
+  
+  divs.append("div")
+    .each(function(d) {
+      if (d.type=='fixed' || d.type=='number') return;
+      // slider type (=default type)
+      $(this).slider({
+        min: 'range' in d ? d.range[0] : 0
+       ,max: 'range' in d ? d.range[1] : d.values.length-1
+       ,value: d.idx
+       ,step: (d.step || 1)
+       ,slide: function(evt, ui) {
+          d.idx = ui.value;
+          update_params(); }
+      });
+    });
+ 
+  update_params();
+
+	$("#stop_correction_on").buttonset();
+	$("#stop_on_worse").bind("click", update_fn);
+	$("#stop_on_conv").bind("click", update_fn);
+};
+
--- examples/joint-chain.js
+++ examples/joint-chain.js
@@ -1,208 +1,189 @@
+/// Written by Erik Weitnauer, 2012.
 /// Simulates a chain of sticks connected by ball joints.
 
-// physics parameters
-var world = new World(new Point(0,10), 10, 10);
-var N = 25,           // number of connected sticks (+ 1 that is fixed)
-    len = 1,          // object length is 1 m
-    mass = 1,         // object mass is 1 kg
-    I = 1/3*mass*len, // inertia of object (langer Stab)
-    timestep = 1/100, // simulation timestep: 10 ms
-    timer_id = null;
-
+// simulation parameters
 var params = {
-  N: {value: 25, range: [1,100], label: "number of objects"}
- ,timestep: {values: [1, 5, 10, 20, 40, 100], idx: 2, postfix:" ms"}
- ,mass: {values: [0.001,0.01,0.1,1,10], idx: 3, postfix: " kg"}
- ,inertia: {values: [0.001,0.01,0.1,0.33,1,10], idx: 3, postfix: " kg*m^2"}
- ,max_err: {values: [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001], idx: 2}
- ,max_verr: {values: [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001], idx: 2}
- ,corr_steps: {label: "max. corr. steps", values: [1,2,3,4,5,10,50,100,1000,10000], idx: 5}
- ,vcorr_steps: {label: "max. v-corr. steps", values: [1,2,3,4,5,10,50,100,1000,10000], idx: 5}
- ,p_factor: {value: 1, range: [0.5, 2], step: 0.1, label: "correction impulse factor"}
- }
-  
-function create_param_array() {
-  var a = [];
-  for (var id in params) {
-    var p = params[id];
-    p.id = id;
-    a.push(p);
-  }
-  return a;
+  N: {value: 10, range: [1,100], label: "chain segments", type: 'number'}
+ ,len: {value: 1, postfix: " m", type: 'fixed', label:"length"}
+ ,mass: {value: 1, postfix: " kg", type: 'fixed'}
+ ,inertia: {value: 0.33, values: [0.001,0.01,0.1,0.33,1,10], postfix: " kg*m^2", type: 'number'}
+ ,timestep: {value: 10, values: [1, 5, 10, 20, 40, 100], postfix:" ms", type: 'number'}
+ ,max_err: {value: 0.001, values: [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001], postfix: " m", label:"pos. error epsilon", type: 'number'}
+ ,max_verr: {value: 0.001, values: [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001], postfix: " m/s", label:"vel. error epsilon", type: 'number'}
+ ,corr_steps: {value: 5, label: "max. pos. corr. steps", values: [1,2,3,4,5,10,50,100,1000,10000], type: 'number'}
+ ,vcorr_steps: {value: 5, label: "max. vel. corr. steps", values: [1,2,3,4,5,10,50,100,1000,10000], type: 'number'}
+ ,p_factor: {value: 1.5, range: [0.5, 2], step: 0.1, label: "scaling of correction impulses", type: 'number'}
+ ,corr_mode: {value: 'below epsilon', label: "stop corr. when error", values: ['below epsilon', 'gets worse'], type: 'number'}
 }
 
-var param_array = create_param_array();
+var len = 1; // each body has a length of 1 m
+var world = new World(new Point(0,10), params.corr_steps.value, params.vcorr_steps.value);
+var timer_id;
 
 // visualization parameters
 var w = 960,    // visualization width in pixels
     h = 600,    // visualization height in pixels
-    r = 5,      // radius of joint visualization in pixels
-    scale = w/2/len/N*0.9, // 1 m in physic = x pixel in visualization
-    body_vis, joint_vis; // d3 selections holding the svg visualizations
+    r = 0.2,    // radius of joint visualization in meter
+    scale = w/2/len/params.N.value*0.9; // 1 m in physic = x pixel in visualization
+
+/// called on page load
+function init() {
+  initVisualization();
+  
+  addSimulationControls(play, pause, step, stop_movement);
+  
+  addParamControls("#sliders", params, update_params);  
+}
+
+/// appends svg element and global g for transformation
+function initVisualization() {
+  var svg = d3.select("#simulation")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+}
+
+function play() {
+  timer_id = setInterval(function() {
+    world.step(params.timestep.value/1000);
+    update();
+  }, params.timestep.value);
+}
+
+function pause() {
+  clearInterval(timer_id);
+}
+
+function step() {
+  world.step(params.timestep.value/1000);
+  update();
+}
+
+function stop_movement() {
+  world.stopMovement();
+  update();
+}
+
+var param_array = create_param_array(params);
 
 var colors = d3.scale.category10();
 
-//hover states on the static widgets
-function addControls() {
-  $('#dialog_link, ul#icons li').hover(
-	  function() { $(this).addClass('ui-state-hover'); }, 
-	  function() { $(this).removeClass('ui-state-hover'); }
-  );
-  $('#play').click(function(evt) {
-    evt.preventDefault();
-    var sp = $("#play span");
-    if (sp.hasClass('ui-icon-play')) {
-      sp.removeClass('ui-icon-play');
-      sp.addClass('ui-icon-pause');
-      $("#play").addClass('ui-state-active');
-      timer_id = setInterval(function() {
-        world.step(timestep);
-        update();
-      }, 1000/100);
-    } else {
-      sp.removeClass('ui-icon-pause');
-      sp.addClass('ui-icon-play');
-      $("#play").removeClass('ui-state-active');
-      clearInterval(timer_id);
-    }
-  });
-  $("#step").click(function(evt) {
-    evt.preventDefault();
-    world.step(timestep);
-    update();
-  });
-  $("#stop_movement").click(function(evt) {
-    evt.preventDefault();
-    world.stopMovement();
-    update();
-  });
-  
-  function update_params() {
-    d3.selectAll("div.parameter")
-      .data(param_array)
-      .select("span")
-      .text(function(d) { 
-        var val = ('values' in d) ? d.values[d.idx] : d.value;
-        var str = ('postfix' in d) ? d.postfix : '';
-        return val + str;
-      });
+function update_params() {
+  // apply parameter values to the simulation
+  // body count should be N+1 (the 1 is the static body at the top)
+  var N = params.N.value;
+  var N_changed = world.bodies.length != N+1;
+  while (world.bodies.length < N+1) addBody();
+  if (world.bodies.length > N+1) {
+    world.bodies.splice(N+1);
+    world.joints.splice(N);
   }
-  
-  var divs = d3.select("#sliders").selectAll("div.parameter")
-    .data(param_array).enter()
-    .append("div")
-    .attr("class", "parameter");
-  
-  divs.append("p")
-    .text(function(d) { return (d.label || d.id) + ': ' })
-    .append("span");
+  if (N_changed) updateN();
     
-  divs.append("div")
-    .attr("id", function(d) { return d.id })
-    .each(function(d) {
-      $("#"+d.id).slider({
-        min: 'range' in d ? d.range[0] : 0
-       ,max: 'range' in d ? d.range[1] : d.values.length-1
-       ,value: 'idx' in d ? d.idx : d.value
-       ,step: (d.step || 1)
-       ,slide: function(evt, ui) {
-          if ('value' in d) d.value = ui.value;
-          else d.idx = ui.value;
-          update_params(); }
-      });
-      update_params();
-    });
- 
-	$( "#stop_correction_on" ).buttonset();
-};
-				
-function init() {
-  var bs = [];
-  bs.push(new Body(new Point(w/scale/2, h*0.05/scale), new Point(), mass, 0, 0, I));
-  bs[0].dynamic = false;
-  world.bodies.push(bs[0]);
-  for (var i=0; i<N; i++) {
-    bs.push(new Body(new Point(w/scale/2 + (i+0.5)*len, h*0.05/scale + len/2), new Point(), mass, -Math.PI*0.5, 0, I));
-    world.bodies.push(bs[i+1]);
-    world.joints.push(new Joint(bs[i], new Point(0,len/2), bs[i+1], new Point(0,-len/2)));
-  }
+  // mass, inertia
+  world.bodies.forEach(function(body) {
+    body.inv_m = 1/params.mass.value;
+    body.inv_I = 1/params.inertia.value;  
+  });
   
-  initVisualization();
-  addControls();
+  // max_err, max_verr, p_factor
+  world.joints.forEach(function(joint) {
+    joint.eps_pos = params.max_err.value;
+    joint.eps_vel = params.max_verr.value;
+    joint.p_factor = params.p_factor.value;
+  });
+  
+  // corr_steps, vcorr_steps
+  world.max_corr_it = params.corr_steps.value;
+  world.max_vcorr_it = params.vcorr_steps.value;
+  
+  // stop on...
+  world.stop_corr_on_worse = $('#stop_on_worse')[0].checked;
+  
+  update();
 }
 
-function initVisualization() {
-  var drag = d3.behavior.drag()
-      .on("drag", dragmove)
-      .on("dragstart", function(d) { d.was_dynamic = d.dynamic; d.dynamic = false; update();})
-      .on("dragend", function(d) {d.dynamic = d.was_dynamic; update();})
-
-  var svg = d3.select("body")
-      .style("background-color", "#444")
-      .select("#simulation")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h)
-    
-//  svg.append("rect")
-//    .attr("x", 0).attr("y", 0).attr("width", w).attr("height", h)
-//    .attr("rx", 6).attr("ry", 6)
-//    .attr("stroke", "#ccc")
-//    .attr("stroke-width", 2)
-////    .attr("fill", "rgb(250,250,255)")
-//    .attr("fill", "#222")
-
-  body_vis = svg.selectAll("g.body")
-        .data(world.bodies)
-        .enter().append("g")
-        .attr("class", "body")
-        .on("click", function(d) {d.dynamic = !d.dynamic; update()});
-
-  body_vis.call(drag);
-  
-  body_vis.append("rect")
-        .attr("x", -4)
-        .attr("y", -len/2*scale-r)
-        .attr("width", 8)
-        .attr("height", len*scale+2*r)
-        .attr("rx", 3)
-        .attr("ry", 3)
-        .attr("stroke", function(d,i) { if (d.dynamic) return colors(i);
-                                        else return "#aaa"; })
-        .attr("fill", function(d,i) { if (d.dynamic) return d3.hsl(colors(i)).brighter();
-                                      else return d3.hsl("#aaa") } )
-        .attr("stroke-width", "3px")
-  
-  joint_vis = svg.selectAll("circle.joint")
-      .data(world.joints)
-      .enter().append("circle")
-      .attr("class", "joint")
-      .attr("fill", "gray")
-      .attr("stroke", "black")
-      .attr("stroke-width", "2px")
-      .attr("r", r-2);
-
-  update();
-  
-  function dragmove(d, i) {
-    d.s.x = Math.max(r, Math.min(w - r, d3.event.x))/scale;
-    d.s.y = Math.max(r, Math.min(h - r, d3.event.y))/scale;
-    update();
+/// Adds one body to the chain. If its not the first one, it gets connected with
+/// a ball joint to the last body.
+function addBody() {
+  // is this the first body in the scene?
+  if (world.bodies.length == 0) {
+    // yes, so make it static
+    var b = new Body(new Point(0, 0), new Point(0, 0),
+                     params.mass.value, 0, 0, params.inertia.value);
+    b.dynamic = false;
+    world.bodies.push(b);
+  } else {
+    // not first body, make it dynamic and link to last body by a ball joint
+    var a = world.bodies[world.bodies.length-1];
+    var pos = a.to_world(new Point(0,len/2));
+    pos.x += len/2;
+    var b = new Body(pos, new Point(), params.mass.value, -Math.PI*0.5, 0, params.inertia.value);
+    world.bodies.push(b);
+    world.joints.push(new Joint(a, new Point(0,len/2), b, new Point(0,-len/2)));  
   }
+}
+
+function dragmove(d, i) {
+  var s = w/2/len/(params.N.value+1);
+  d.s.x += d3.event.dx/s;
+  d.s.y += d3.event.dy/s;
+  update();
+}
+
+var drag_body = d3.behavior.drag()
+  .on("drag", dragmove)
+  .on("dragstart", function(d) { d.was_dynamic = d.dynamic; d.dynamic = false; update();})
+  .on("dragend", function(d) {d.dynamic = d.was_dynamic; update();})
+
+var bs, js;
+
+function updateN() {
+  bs = d3.select("svg").selectAll("rect.body").data(world.bodies);
+  var s = w/2/len/(params.N.value+1);
+  var N = params.N.value;
+  
+  bs.exit().remove();
+
+  bs.enter().append("rect")
+    .on("click", function(d,i) { d.dynamic = !d.dynamic; update(); })
+    .attr("class", "body")
+    .call(drag_body);
+  
+  bs.attr("x", Math.min(-1,s*(-r/2)))
+    .attr("y", s*(-len/2-r/2))
+    .attr("width", Math.max(2,s*r))
+    .attr("height", s*(len+r))
+    .attr("rx", N>20 ? null : s*r/2)
+    .attr("ry", N>20 ? null : s*r/2)
+    .attr("stroke-width", s*r/4)
+  
+  js = d3.select("svg").selectAll("circle.joint").data(world.joints);
+  
+  js.exit().remove();
+  
+  js.enter().append("circle")
+    .attr("class", "joint")
+    .attr("fill", "gray")
+    .attr("stroke", "black");
+    
+  js.attr("stroke-width", s*r/4)
+    .attr("r", s*r/4);
 }
 
 function update() {
-  body_vis
-    .attr("transform", function(d) {
-      return 'translate(' + d.s.x*scale + ',' + d.s.y*scale +
-             ') rotate(' + 180/Math.PI*d.r + ")" })
-    .select("rect")
-    .attr("stroke", function(d,i) { if (d.dynamic) return colors(i);
-                                        else return "#aaa"; })
-    .attr("fill", function(d,i) { if (d.dynamic) return d3.hsl(colors(i)).brighter();
-                                      else return d3.hsl("#aaa") } )
-             
-  joint_vis
-    .attr("cx", function(d) { return d.aInWorld().x*scale; })
-    .attr("cy", function(d) { return d.aInWorld().y*scale; })
+  var s = w/2/len/(params.N.value+1);
+  var N = params.N.value;
+  
+  bs.attr("transform", function(d) {
+           return 'translate(' + (s*d.s.x+w/2) + ',' + s*(d.s.y+len) + ') ' +
+                  'rotate(' + 180/Math.PI*d.r + ') '})
+    .attr("stroke", N>20 ? "none" : function(d,i)
+         { return d.dynamic ? colors(i) : "#eee" })
+    .attr("fill", function(d,i)
+         { return d.dynamic ? d3.hsl(colors(i)).brighter() : "#eee" })
+
+  js.style("display", N>20 ? "none" : null)
+    .attr("cx", function(d) { return d.aInWorld().x*s+w/2; })
+    .attr("cy", function(d) { return d.aInWorld().y*s+s*len; });
 }
